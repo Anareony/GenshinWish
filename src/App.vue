6525c1d478a95b9b0e87b8f09c2033b2fcead06e
Anareony <chudoyakov.roman@gmail.com>
2022-02-24 04:08:33 +0700

    minor changes

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,12 @@
 <template>
     <div class="app">
-        <h1>Страница с постами</h1>
-        <my-button
-            @click="showDialog"
-            style="margin: 15px 0;"  
-        >
-            Создать пост
-        </my-button>
-        <my-dialog v-model:show="dialogVisible">
-            <post-form @create="createPost"/>
-        </my-dialog>
-        <post-list
-            :posts="posts"
-            @remove="removePost"
-        />
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-import PostForm from '@/components/PostForm'
-import PostList from '@/components/PostList'
-import MyDialog from './components/UI/MyDialog.vue'
-import MyButton from './components/UI/MyButton.vue'
-
 export default {
-    components: {
-    PostList,
-    PostForm,
-    MyDialog,
-    MyButton
-},
-    data() {
-        return {
-            posts: [
-                {id: 1, title: 'JS', body: 'Описание поста'},
-                {id: 2, title: 'JS 2', body: 'Описание поста 2'},
-                {id: 3, title: 'JS 3', body: 'Описание поста 3'},
-            ],
-            dialogVisible: false
-        }
-    },
-    methods: {
-        createPost(post) {
-            this.posts.push(post);
-            this.dialogVisible = false;
-        },
-        removePost(post) {
-            this.posts = this.posts.filter(p => p.id !== post.id);
-        },
-        showDialog() {
-            this.dialogVisible = true;
-        }
-    }
+
 }
 </script>
 
@@ -63,9 +18,5 @@ export default {
 }
 .app {
     margin: 15px;
-}
-form {
-    display: flex;
-    flex-direction: column;
 }
 </style>
