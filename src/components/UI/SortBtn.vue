040574c676ab36739e04a21557b00f5a26856c7e
Anareony <chudoyakov.roman@gmail.com>
2022-03-02 23:58:32 +0700

    add sort btn

---
+++ src/components/UI/SortBtn.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="sort-btn"></button>
+</template>
+
+<script>
+export default {
+  name: 'sort-btn',
+
+}
+</script>
+
+<style scoped>
+  .sort-btn{
+    width: 50px;
+    height: 50px;
+    background: white;
+  }
+</style>
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,11 +1,13 @@
 import MyButton from "@/components/UI/MyButton.vue";
 import MyInput from "@/components/UI/MyInput.vue";
 import MyDialog from "@/components/UI/MyDialog";
-import MySelect from "@/components/UI/MySelect"
+import MySelect from "@/components/UI/MySelect";
+import SortBtn from "@/components/UI/SortBtn"
 
 export default [
     MyButton,
     MyInput,
     MyDialog,
-    MySelect
+    MySelect,
+    SortBtn
 ]
