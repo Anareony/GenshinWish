0081953c27389adcf88e80a8b5b3d667e87b607c
Anareony <chudoyakov.roman@gmail.com>
2022-03-04 09:43:24 +0700

    add wish counter

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import Main from '@/pages/Main'
 import Characters from '@/pages/Characters'
 import CharacterPage from '@/pages/CharacterPage'
+import Wishcounter from '@/pages/WishCounter'
 import Calculator from '@/pages/Calculator'
 import PostPage from '@/pages/PostPage'
 import { createWebHashHistory, createRouter } from 'vue-router'
@@ -17,6 +18,10 @@ const routes = [
     {
         path: '/characters/:name',
         component: CharacterPage,
+    },
+    {
+        path: '/wishcounter',
+        component: Wishcounter,
     },
     {
         path: '/calculator',
